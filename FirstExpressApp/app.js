9919b07fa5978c1dff220d651f94338d344130cb
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-15 10:53:55 +1100

    Played around with some dynamic routing

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -15,6 +15,18 @@ app.get('/cat', (req, res) => {
   res.send('MEOW!');
 });
 
+// Pattern route (use : to designate a variable in the route)
+app.get('/r/:subredditName', (req, res) => {
+  const subreddit = req.params.subredditName;
+
+  res.send(`Welcome to the ${subreddit.toUpperCase()} subreddit`);
+});
+
+// Catchall route (ex: 404 page)
+app.get('*', (req, res) => {
+  res.send('YOU ARE A STAR');
+});
+
 // Tell Express to listen for requests (start server)
 app.listen(3000, () => {
   console.log('Server has started!');
